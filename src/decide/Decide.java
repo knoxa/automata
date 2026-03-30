774ec4a5240fe02ef32fbc44bb693d7f938534a2
knoxa <swaffhamtown@outlook.com>
2026-03-30 10:44:57 +0100

    removed redundant method

--- src/decide/Decide.java
+++ src/decide/Decide.java
@@ -1,7 +1,6 @@
 package decide;
 
 import java.util.ArrayList;
-import java.util.Comparator;
 import java.util.HashSet;
 import java.util.List;
 import java.util.Map;
@@ -9,7 +8,6 @@ import java.util.Random;
 import java.util.Set;
 
 import act.Action;
-import cells.Square;
 
 public class Decide {
 
@@ -44,34 +42,6 @@ public class Decide {
 				
 				if ( random.nextDouble() < action.getProbability() )  choices.add(action);
 			}
-		}
-		
-		return choices;
-	}
-	
-	
-	public static Set<Action> acceptX(Map<Square, Set<Action>> actions, Random random) {
-		
-		// filter a map of actions keyed by square
-		// no rules about partitions applied here ...
-		
-		Set<Action> choices = new HashSet<Action>();
-		
-		for ( Square square: actions.keySet() ) {
-			
-			List<Action> options = new ArrayList<Action>();
-			options.addAll(actions.get(square));
-			
-			options.sort(new Comparator<Action> () {
-
-				public int compare(Action arg0, Action arg1) {
-					return Double.compare(arg1.getProbability(), arg0.getProbability()); // ascending
-				}
-			});
-			
-			//int n = random.nextInt(options.size());
-			Action action = options.get(0);  // most probable action
-			if ( random.nextDouble() < action.getProbability() )  choices.add(action);
 		}
 		
 		return choices;
